7ca865d4796415142513faa7ed2e99baad7ff29a
Grant Williams <greatgrant116@gmail.com>
2021-06-19 08:27:39 +0100

    Refactored code to reduce use of findviewbyId

--- app/src/main/java/com/gralliams/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/gralliams/mydiceroller/MainActivity.kt
@@ -3,15 +3,18 @@ package com.gralliams.mydiceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var resultImageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        resultImageView = findViewById(R.id.result_imageview)
         val rollButton: Button = this.findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
@@ -19,8 +22,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        var numberTextView: TextView = findViewById(R.id.result_textview)
         var randomInt = Random.nextInt(6) + 1
-        numberTextView.text = randomInt.toString()
+        var imageResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        resultImageView.setImageResource(imageResource)
     }
 }
